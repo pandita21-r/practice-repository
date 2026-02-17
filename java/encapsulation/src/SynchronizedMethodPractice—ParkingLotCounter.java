cd946bf97adc46f70621aa6654b21528356cc037
Raymark Pandita <raymarkpandita21@gmail.com>
2026-02-17 21:43:49 +0800

    Create SynchronizedMethodPractice—ParkingLotCounter.java

---
+++ java/encapsulation/src/SynchronizedMethodPractice—ParkingLotCounter.java
@@ -0,0 +1,44 @@
+package package1;
+
+public class TestRoom10 {
+    static class ParkingLot{
+        private int capacity;
+        private int carsInside;
+
+        public ParkingLot(int capacity){
+            if(capacity <= 0){
+                throw new IllegalArgumentException("Invalid capacity");
+            }
+            this.capacity = capacity;
+        }
+        public synchronized void enter(){
+            if(carsInside == capacity){
+                throw new IllegalArgumentException("Parking lot is full.");
+            }
+            carsInside += 1;
+            System.out.println("One car entered.");
+        }
+        public synchronized void exit(){
+            if(carsInside == 0){
+                throw new IllegalArgumentException("Parking lot is empty");
+            }
+            carsInside -= 1;
+            System.out.println("One car exited.");
+        }
+        public int getCarsInside(){
+            return carsInside;
+        }
+    }
+    public static void main(String[] args) {
+        ParkingLot p = new ParkingLot(3);
+
+        try{
+            p.enter();
+            p.enter();
+            p.exit();
+            System.out.println(p.getCarsInside());
+        } catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
