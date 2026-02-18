ee2fcd94e47445d4e536904cdaefb55a76cc16e2
Raymark Pandita <raymarkpandita21@gmail.com>
2026-02-18 22:41:33 +0800

    Create InheritancePractice—Animal,Dog,Cat.java

---
+++ java/inheritance/src/InheritancePractice—Animal,Dog,Cat.java
@@ -0,0 +1,55 @@
+package package1;
+
+public class TestRoom9 {
+    static class Animal {
+
+        protected String name;
+        protected int age;
+
+        public Animal(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String describe() {
+            return "Name: " + name + " Age: " + age + ",";
+        }
+    }
+
+    static class Dog extends Animal {
+        private String breed;
+
+        public Dog(String name, int age, String breed) {
+            super(name, age);
+            this.breed = breed;
+        }
+
+        @Override
+        public String describe() {
+            return super.describe() + " Breed: " + breed;
+        }
+    }
+
+    static class Cat extends Animal {
+        private boolean isIndoor;
+
+        public Cat(String name, int age, boolean isIndoor) {
+            super(name, age);
+            this.isIndoor = isIndoor;
+        }
+
+        @Override
+        public String describe() {
+            String indoorText = isIndoor ? "Indoor" : "Outdoor";
+            return super.describe() + " Type: " + indoorText;
+        }
+    }
+
+    public static void main(String[] args) {
+        Dog d = new Dog("Rocky", 4, "Golden Retriever");
+        Cat c = new Cat("Luna", 2, true);
+
+        System.out.println(d.describe());
+        System.out.println(c.describe());
+    }
+}
