a55b54754ee10ec6751457412ee47dc0ce4f6541
Raymark Pandita <raymarkpandita21@gmail.com>
2026-03-03 11:46:16 +0800

    Implement more function to the button

--- java/javaswing/src/MyButton.java
+++ java/javaswing/src/MyButton.java
@@ -3,16 +3,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 
 public class MyButton extends JFrame implements ActionListener {
 
     JButton button;
+    JLabel label;
 
     MyButton() {
 
         ImageIcon icon = new ImageIcon("java/javaswing/SensationalIcon.png");
+        ImageIcon icon2 = new ImageIcon("java/javaswing/SensationalIcon2.png");
+
+        label = new JLabel();
+        label.setIcon(icon2);
+        label.setBounds(150, 250, 150, 150);
+        label.setVisible(false); // Set to false first so that when you clicked the button the it will become set to true and become visible
 
         button = new JButton();
         button.setBounds(140, 100, 190, 150); // button sizing
@@ -34,6 +42,7 @@ public class MyButton extends JFrame implements ActionListener {
         this.setSize(500, 500);
         this.setVisible(true);
         this.add(button);
+        this.add(label);
     }
 
     @Override
@@ -41,6 +50,7 @@ public class MyButton extends JFrame implements ActionListener {
         if (e.getSource() == button) {
             System.out.println("Sensational");
         // button.setEnabled(false); - Disable the button after you click it once
+            label.setVisible(true); // When we click the button the label that we add will appear.
         }
     }
 }
