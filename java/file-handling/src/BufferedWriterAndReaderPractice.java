5d41e314b3b0d016dff2ba3e678e992349b9c13b
Raymark Pandita <raymarkpandita21@gmail.com>
2026-03-03 21:59:59 +0800

    Practice reading file and display it on the console

--- java/file-handling/src/BufferedWriterAndReaderPractice.java
+++ java/file-handling/src/BufferedWriterAndReaderPractice.java
@@ -1,8 +1,10 @@
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 
-public class BufferedReaderPractice {
+public class BufferedWriterAndReaderPractice {
     public static void main(String[] args) {
 
         int[] numbers = { 1, 2, 3, 4 };
@@ -17,6 +19,16 @@ public class BufferedReaderPractice {
             System.out.println("File written successfully.");
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("practice.txt"))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Could not read the file: " + e.getMessage());
         }
     }
 }
