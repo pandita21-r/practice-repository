f92717431549ef62bb05b92cebd1257cfb774eb3
Raymark Pandita <raymarkpandita21@gmail.com>
2026-03-04 10:42:05 +0800

    Prevent instantiation for write to file class

--- java/file-handling/src/WriteToFile.java
+++ java/file-handling/src/WriteToFile.java
@@ -2,17 +2,22 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+        // Utility class
+        
 public class WriteToFile {
+    private WriteToFile() {
+        // Prevent instantiation
+    }
 
     public static void write(String fileName, String[] data) {
         if (fileName == null || fileName.isBlank()) {
             throw new IllegalArgumentException("File name cannot be null or blank.");
         }
-        if (data == null) {
-            throw new IllegalArgumentException("Data cannot be null or blank.");
+        if (data == null || data.length == 0) {
+            throw new IllegalArgumentException("Data cannot be null or empty.");
         }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) { // Add "true" for append mode
             for (String line : data) {
                 writer.write(line);
                 writer.newLine();
