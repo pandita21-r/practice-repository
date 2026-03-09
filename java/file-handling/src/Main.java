e16c850435a55b385271556a18134f03fb664b12
Raymark Pandita <raymarkpandita21@gmail.com>
2026-03-10 00:18:16 +0800

    Added new file for Pyhton practice.

--- java/file-handling/src/Main.java
+++ java/file-handling/src/Main.java
@@ -1,10 +1,22 @@
+
 public class Main {
     public static void main(String[] args) {
 
-        String[] data = { "Cat", "Dog", "Bird", "Fish" };
+         String[] data = { "Cat", "Dog", "Bird", "Fish" };
+         WriteToFile.write("data.txt", data);
+         ReadFile.read("data.txt");
+         System.out.println("Data successfully stored.");
 
-        WriteToFile.write("data.txt", data);
-        ReadFile.read("data.txt");
-        System.out.println("Data successfully stored.");
+        // ArrayList<String> list = new ArrayList<>();
+
+        // list.add("Tomato");
+        // list.add("Carrot");
+        // list.add("Onion");
+        // list.add("Garlic");
+        // list.add("Corn");
+
+        // for (String crops : list) {
+        // System.out.println(crops);
+        // }
     }
 }
---
+++ python/first_python_program.py
@@ -0,0 +1,5 @@
+# Unang first python program 3/10/26 12:16 AM
+
+text = "My first python program."
+num = 67
+print(f"{text} MWEHEHEHE! {num}!{num}!{num}!")
