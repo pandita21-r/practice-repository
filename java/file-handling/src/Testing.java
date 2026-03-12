a6415191f354de281f85222ebb1d58958757a212
Raymark Pandita <raymarkpandita21@gmail.com>
2026-03-12 23:21:10 +0800

    File handling practice session.

---
+++ java/file-handling/src/Testing.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Testing {
+    public static void main(String[] args) {
+
+        String[] foods = { "Hotdog", "Pizza", "Fries" };
+        File file = new File("file.txt");
+
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occured" + e.getMessage());
+        }
+
+        try (FileWriter writer = new FileWriter(file)) {
+            for (String line : foods) {
+                writer.write(line + "\n");
+            }
+            System.out.println("Successfully write to " + file.getName());
+        } catch (IOException e) {
+            System.out.println("An error occured" + e.getMessage());
+        }
+        try (FileReader reader = new FileReader(file)) {
+            BufferedReader rd = new BufferedReader(reader);
+            String line;
+            while ((line = rd.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occured" + e.getMessage());
+        }
+    }
+}
