c5281ce8c425faf0e964216e9bba216c43034a77
Raymark Pandita <raymarkpandita21@gmail.com>
2026-03-13 20:32:31 +0800

    Practice manually adding components to the frame

--- java/javaswing/testroom/Main.java
+++ java/javaswing/testroom/Main.java
@@ -2,7 +2,6 @@ package javaswing.testroom;
 
 public class Main {
     public static void main(String[] args) {
-
         UI.start();
     }
 }
--- java/javaswing/testroom/UI.java
+++ java/javaswing/testroom/UI.java
@@ -1,33 +1,41 @@
 package javaswing.testroom;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class UI {
 
     public static void start() {
+        JLabel label1 = new JLabel();
+
+        label1.setText("Hello!");
+        label1.setBackground(Color.DARK_GRAY);
+        label1.setForeground(new Color(177, 3, 252));
+        label1.setFont(new Font("MV Boli", Font.BOLD, 20));
+
+        label1.setOpaque(true);
+
+        label1.setBounds(15, 20, 100, 80);
+        label1.setHorizontalAlignment(JLabel.CENTER);
+
+        JPanel panel1 = new JPanel();// JPanel's layout is FLowLayout by default! set it to null if you want your set bounds to work!
+
+        panel1.setLayout(null);
+        panel1.setBackground(Color.GREEN);
+        panel1.setBounds(0, 0, 140, 120);
+        panel1.add(label1);
+
         JFrame frame = new JFrame("Input");
         frame.setSize(420, 420);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(true);
+        frame.setResizable(false);
 
-        JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new BorderLayout());
-        inputPanel.setBackground(Color.WHITE);
-        inputPanel.setBounds(100, 110, 250,100);
+        frame.add(panel1);
 
-        JLabel nameLabel = new JLabel("Username: ");
-        JTextField nameField = new JTextField(15);
-
-        inputPanel.add(nameLabel, BorderLayout.WEST);
-        inputPanel.add(nameField, BorderLayout.CENTER);
-
-        frame.add(inputPanel);
         frame.setVisible(true);
     }
 }
