03a62c34c13f7f1c8fabe09e85ab6fced635dec4
Raymark Pandita <raymarkpandita21@gmail.com>
2026-03-15 09:03:45 +0800

    Added border line for every panel

--- java/javaswing/testroom/BorderLayoutDemo.java
+++ java/javaswing/testroom/BorderLayoutDemo.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,27 +22,33 @@ public class BorderLayoutDemo {
         // Panels
         JPanel northPanel = new JPanel();
         northPanel.add(label1);
-        northPanel.setPreferredSize(new Dimension(0,50));
+        northPanel.setPreferredSize(new Dimension(0,100));
         northPanel.setBackground(Color.BLUE);
+        northPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         JPanel southPanel = new JPanel();
-        southPanel.setPreferredSize(new Dimension(0,50));
+        southPanel.setPreferredSize(new Dimension(0,100));
         southPanel.add(label2);
         southPanel.setBackground(Color.RED);
+        southPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         JPanel eastPanel = new JPanel();
-        eastPanel.setPreferredSize(new Dimension(50,0));
+        eastPanel.setPreferredSize(new Dimension(200,0));
         eastPanel.add(label3);
         eastPanel.setBackground(Color.MAGENTA);
+        eastPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         JPanel westPanel = new JPanel();
-        westPanel.setPreferredSize(new Dimension(50,0));
+        westPanel.setPreferredSize(new Dimension(200,0));
         westPanel.add(label4);
         westPanel.setBackground(Color.GREEN);
+        westPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         JPanel centerPanel = new JPanel();
         // No "setPreferredSize" because its centered.
         centerPanel.add(label5);
         centerPanel.setBackground(Color.WHITE);
+        centerPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         JFrame frame = new JFrame();
         frame.setLayout(new BorderLayout());
+        frame.setSize(400,400);
         frame.setResizable(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
