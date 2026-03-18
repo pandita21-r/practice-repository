db6820cfaec4b54b28191b2257b846faea1f5f79
Raymark Pandita <raymarkpandita21@gmail.com>
2026-03-18 20:13:37 +0800

    Flow layout practice session

--- java/javaswing/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    @SuppressWarnings("all")
-    public static void main(String[] args) {
-        LaunchPage launchPage = new LaunchPage();
-    }
-}
--- java/javaswing/testroom/UI.java
+++ java/javaswing/testroom/UI.java
@@ -1,41 +1,56 @@
 package javaswing.testroom;
 
-import java.awt.Color;
-import java.awt.Font;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 
 public class UI {
-
     public static void start() {
-        JLabel label1 = new JLabel();
+        JPanel nPanel = new JPanel();
+        nPanel.setBackground(Color.GREEN);
+        nPanel.setPreferredSize(new Dimension(0, 90));
 
-        label1.setText("Hello!");
-        label1.setBackground(Color.DARK_GRAY);
-        label1.setForeground(new Color(177, 3, 252));
-        label1.setFont(new Font("MV Boli", Font.BOLD, 20));
+        JPanel sPanel = new JPanel();
+        sPanel.setBackground(Color.MAGENTA);
+        sPanel.setPreferredSize(new Dimension(0, 90));
 
-        label1.setOpaque(true);
+        JPanel ePanel = new JPanel();
+        ePanel.setBackground(Color.YELLOW);
+        ePanel.setPreferredSize(new Dimension(70, 0));
 
-        label1.setBounds(15, 20, 100, 80);
-        label1.setHorizontalAlignment(JLabel.CENTER);
+        JPanel wPanel = new JPanel();
+        wPanel.setBackground(Color.BLACK);
+        wPanel.setPreferredSize(new Dimension(70, 0));
 
-        JPanel panel1 = new JPanel();// JPanel's layout is FLowLayout by default! set it to null if you want your set bounds to work!
+        JButton btn1 = new JButton("Button 1");
+        JButton btn2 = new JButton("Button 2");
+        JButton btn3 = new JButton("Button 3");
+        JButton btn4 = new JButton("Button 4");
+        JButton btn5 = new JButton("Button 5");
+        JButton btn6 = new JButton("Button 6");
 
-        panel1.setLayout(null);
-        panel1.setBackground(Color.GREEN);
-        panel1.setBounds(0, 0, 140, 120);
-        panel1.add(label1);
+        JPanel cPanel = new JPanel();
+        // Flow Layout teaches or automatically fix the positions of components.
+        cPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        cPanel.setBackground(Color.LIGHT_GRAY);
+        cPanel.add(btn1);
+        cPanel.add(btn2);
+        cPanel.add(btn3);
+        cPanel.add(btn4);
+        cPanel.add(btn5);
+        cPanel.add(btn6);
 
         JFrame frame = new JFrame("Practice");
         frame.setSize(420, 420);
-        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(new BorderLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
 
-        frame.add(panel1);
-
+        frame.add(nPanel, BorderLayout.NORTH);
+        frame.add(sPanel, BorderLayout.SOUTH);
+        frame.add(ePanel, BorderLayout.EAST);
+        frame.add(wPanel, BorderLayout.WEST);
+        frame.add(cPanel, BorderLayout.CENTER);
         frame.setVisible(true);
     }
 }
