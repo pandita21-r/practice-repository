372eabefec252216df7e3ed0c26daafd44f1757c
Raymark Pandita <raymarkpandita21@gmail.com>
2026-05-28 20:18:45 +0800

    Python basic syntax practice session

---
+++ python/src/indentation.py
@@ -0,0 +1,7 @@
+num1 = 2
+num2 = 5
+
+if num1 < num2:
+    print("Indentation is important on python") # Proper indentation is important for python syntax.
+if num1 < num2:
+        print("Hello, World!") # Proper indent again!
---
+++ python/src/printingwithoutanewline.py
@@ -0,0 +1,4 @@
+# Printing without a new line - , end=" "
+
+print("I am printing without a new line.", end=" ")
+print("This will print on the same line.")
