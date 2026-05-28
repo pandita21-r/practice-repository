6645b119164820378c2e4ebd784b974dba914326
Raymark Pandita <raymarkpandita21@gmail.com>
2026-05-28 20:33:39 +0800

    Fixed file naming

--- python/src/printingwithoutanewline.py
+++
@@ -1,4 +0,0 @@
-# Printing without a new line - , end=" "
-
-print("I am printing without a new line.", end=" ")
-print("This will print on the same line.")
---
+++ python/src/properprinting.py
@@ -0,0 +1,21 @@
+# Printing without a new line - , end=" "
+
+print("I am printing without a new line.", end=" ")
+print("This will print on the same line.")
+
+# Printing numbers. 
+# Can also do math when printing. 
+
+print(3)
+print(300)
+print(3000)
+
+print(3 + 3)
+print(2 * 5)
+print(100 + 3000)
+
+# Mix them (Numbers and Texts)
+# Can also Math
+
+print("I am", 21, "years old.")
+print("This sum is", 20 + 5)
