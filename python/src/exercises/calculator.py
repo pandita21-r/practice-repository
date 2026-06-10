6c158d8cd877e26ca7c0e360b0d0b752207d2942
Raymark Pandita <raymarkpandita21@gmail.com>
2026-06-10 18:34:22 +0800

    Python practice session.

---
+++ python/src/exercises/calculator.py
@@ -0,0 +1,21 @@
+# Simple python calculator
+
+operator = input("Enter an operator (+ - * /): ")
+
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result))
+else:
+    print(f"{operator} is not a valid operator!")
